d799892ee947e2fcb01ca19c599195c213e09242
TNGGG <linh3712@gmail.com>
2024-10-28 15:59:55 +0700

    Move files

---
+++ src/test/java/Admin/Login/T2MFALogin.java
@@ -0,0 +1,10 @@
+package Admin.Login;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class T2MFALogin {
+    @BeforeTest
+    public void nem(){}
+    @Test
+}
